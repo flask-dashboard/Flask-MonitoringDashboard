83013901c0fa6e691df844c5e2fcdb34aecc70da
Patrick <patrickvogel@live.nl>
2017-05-30 13:22:12 +0200

    Fixed more broken urls and fixed the issue with not storing changes in dashboard/rules

--- flask-dashboard/dashboard/measurement.py
+++ flask-dashboard/dashboard/measurement.py
@@ -18,21 +18,23 @@ def init_measurement():
     It adds wrappers to the endpoints for tracking their performance and last access times.
     """
     for rule in get_monitor_rules():
-        user_app.view_functions[rule.endpoint] = track_performance(user_app.view_functions[rule.endpoint])
+        user_app.view_functions[rule.endpoint] = track_performance(user_app.view_functions[rule.endpoint],
+                                                                   endpoint=rule.endpoint)
 
     # filter dashboard rules
     rules = user_app.url_map.iter_rules()
     rules = [r for r in rules if not r.rule.startswith('/' + config.link)
              and not r.rule.startswith('/static-' + config.link)]
     for rule in rules:
-        user_app.view_functions[rule.endpoint] = track_last_accessed(user_app.view_functions[rule.endpoint])
+        user_app.view_functions[rule.endpoint] = track_last_accessed(user_app.view_functions[rule.endpoint],
+                                                                     endpoint=rule.endpoint)
 
 
-def track_performance(func):
+def track_performance(func, endpoint):
     """
     Measure the execution time of a function and store result in the database
     :param func: the function to be measured
-    :return: 
+    :param endpoint: the name of the endpoint
     """
     @wraps(func)
     def wrapper(*args, **kwargs):
@@ -40,16 +42,20 @@ def track_performance(func):
         result = func(*args, **kwargs)
         time2 = time.time()
         t = (time2-time1)*1000
-        add_function_call(time=t, endpoint=func.__name__)
+        add_function_call(time=t, endpoint=endpoint)
         return result
     wrapper.original = func
     return wrapper
 
 
-def track_last_accessed(func):
-    """ Keep track of the last access time of the endpoints. """
+def track_last_accessed(func, endpoint):
+    """
+    Keep track of the last access time of the endpoints. 
+    :param func: the function to be measured
+    :param endpoint: the name of the endpoint
+    """
     @wraps(func)
     def wrapper(*args, **kwargs):
-        update_last_accessed(endpoint=func.__name__, value=datetime.datetime.now())
+        update_last_accessed(endpoint=endpoint, value=datetime.datetime.now())
         return func(*args, **kwargs)
     return wrapper
