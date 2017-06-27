de90164acfa57b6dd870c60edbd4c7dc09829d2a
Patrick <patrickvogel@live.nl>
2017-06-27 10:21:55 +0200

    Updated setup-version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="flask_monitoring_dashboard",
-    version="1.6",
+    version="1.7",
     packages=setuptools.find_packages(),
     include_package_data=True,
     zip_safe=False,
