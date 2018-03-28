2641d36a13f02fdfcc7eba2ace56407653118af5
flyingbird95 <patrickvogel@live.nl>
2018-03-28 11:30:22 +0200

    removed Endpoint from axis

--- flask_monitoringdashboard/routings/measurements.py
+++ flask_monitoringdashboard/routings/measurements.py
@@ -244,7 +244,7 @@ def get_version_usage():
         showlegend=False,
         title='Heatmap of hits per endpoint per version',
         xaxis=go.XAxis(title='Versions', type='category'),
-        yaxis=dict(title='Endpoints', type='category', autorange='reversed'),
+        yaxis=dict(type='category', autorange='reversed'),
         margin=go.Margin(
             l=200
         )
