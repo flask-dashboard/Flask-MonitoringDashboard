c90f14295530da8ea8f4d2b473b2d6132f60b991
Patrick Vogel <patrickvogel@live.nl>
2018-05-08 20:02:24 +0200

    created application with 31 endpoints

--- flask_monitoringdashboard/main.py
+++ flask_monitoringdashboard/main.py
@@ -12,23 +12,134 @@ def create_app():
     app = Flask(__name__)
 
     dashboard.config.outlier_detection_constant = 0
-    dashboard.config.group_by = 'User', lambda: 3
+    dashboard.config.group_by = 'User', 1
     dashboard.config.version = 1.0
-    dashboard.config.database_name = 'sqlite:///flask_monitoringdashboard.db'
-    # dashboard.config.database_name = 'mysql+pymysql://root@localhost/flask_test'
-    dashboard.bind(app=app)
+    dashboard.config.database_name = 'mysql+pymysql://root@localhost/flask_test'
+    dashboard.bind(app)
+
+    @app.route('/endpoint1')
+    def endpoint1():
+        return redirect(url_for('dashboard.index'))
+
+    @app.route('/endpoint2')
+    def endpoint2():
+        return redirect(url_for('dashboard.index'))
+
+    @app.route('/endpoint3')
+    def endpoint3():
+        return redirect(url_for('dashboard.index'))
+
+    @app.route('/endpoint4')
+    def endpoint4():
+        return redirect(url_for('dashboard.index'))
+
+    @app.route('/endpoint5')
+    def endpoint5():
+        return redirect(url_for('dashboard.index'))
+
+    @app.route('/endpoint6')
+    def endpoint6():
+        return redirect(url_for('dashboard.index'))
+
+    @app.route('/endpoint7')
+    def endpoint7():
+        return redirect(url_for('dashboard.index'))
+
+    @app.route('/endpoint8')
+    def endpoint8():
+        return redirect(url_for('dashboard.index'))
+
+    @app.route('/endpoint9')
+    def endpoint9():
+        return redirect(url_for('dashboard.index'))
+
+    @app.route('/endpoint10')
+    def endpoint10():
+        return redirect(url_for('dashboard.index'))
+
+    @app.route('/endpoint11')
+    def endpoint11():
+        return redirect(url_for('dashboard.index'))
+
+    @app.route('/endpoint12')
+    def endpoint12():
+        return redirect(url_for('dashboard.index'))
+
+    @app.route('/endpoint13')
+    def endpoint13():
+        return redirect(url_for('dashboard.index'))
+
+    @app.route('/endpoint14')
+    def endpoint14():
+        return redirect(url_for('dashboard.index'))
+
+    @app.route('/endpoint15')
+    def endpoint15():
+        return redirect(url_for('dashboard.index'))
+
+    @app.route('/endpoint16')
+    def endpoint16():
+        return redirect(url_for('dashboard.index'))
+
+    @app.route('/endpoint17')
+    def endpoint17():
+        return redirect(url_for('dashboard.index'))
+
+    @app.route('/endpoint18')
+    def endpoint18():
+        return redirect(url_for('dashboard.index'))
+
+    @app.route('/endpoint19')
+    def endpoint19():
+        return redirect(url_for('dashboard.index'))
+
+    @app.route('/endpoint20')
+    def endpoint20():
+        return redirect(url_for('dashboard.index'))
+
+    @app.route('/endpoint21')
+    def endpoint21():
+        return redirect(url_for('dashboard.index'))
+
+    @app.route('/endpoint22')
+    def endpoint22():
+        return redirect(url_for('dashboard.index'))
+
+    @app.route('/endpoint23')
+    def endpoint23():
+        return redirect(url_for('dashboard.index'))
+
+    @app.route('/endpoint24')
+    def endpoint24():
+        return redirect(url_for('dashboard.index'))
+
+    @app.route('/endpoint25')
+    def endpoint25():
+        return redirect(url_for('dashboard.index'))
+
+    @app.route('/endpoint26')
+    def endpoint26():
+        return redirect(url_for('dashboard.index'))
+
+    @app.route('/endpoint27')
+    def endpoint27():
+        return redirect(url_for('dashboard.index'))
+
+    @app.route('/endpoint28')
+    def endpoint28():
+        return redirect(url_for('dashboard.index'))
+
+    @app.route('/endpoint29')
+    def endpoint29():
+        return redirect(url_for('dashboard.index'))
+
+    @app.route('/endpoint30')
+    def endpoint30():
+        return redirect(url_for('dashboard.index'))
 
     @app.route('/')
     def main():
         return redirect(url_for('dashboard.index'))
-
-    @app.route('/endpoint')
-    def endpoint():
-        return redirect(url_for('dashboard.index'))
-
-    @app.route('/outl')
-    def outl():
-        return redirect('sfsef')
 
     return app
 
