954a36448d61b8f829da161b866a49f535ae53d9
Patrick Vogel <patrickvogel@live.nl>
2018-05-10 13:29:40 +0200

    refactored database function in two functions

--- flask_monitoringdashboard/database/endpoint.py
+++ flask_monitoringdashboard/database/endpoint.py
@@ -102,14 +102,17 @@ def update_monitor_rule(db_session, endpoint, value):
         update({MonitorRule.monitor: value})
 
 
+def to_local_datetime(datetime):
+    """ Convert datetime (UTC) to local datetime from the configuration """
+    if datetime:
+        return datetime.replace(tzinfo=pytz.utc).astimezone(config.timezone)
+    return None
+
+
 def get_last_accessed_times(db_session):
     """ Returns the accessed time of a single endpoint. """
     result = db_session.query(MonitorRule.endpoint, MonitorRule.last_accessed).all()
-    values = []
-    for end, time in result:
-        if time:
-            values.append((end, time.replace(tzinfo=pytz.utc).astimezone(config.timezone)))
-    return values
+    return [(end, to_local_datetime(time)) for end, time in result]
 
 
 def update_last_accessed(db_session, endpoint, value):
