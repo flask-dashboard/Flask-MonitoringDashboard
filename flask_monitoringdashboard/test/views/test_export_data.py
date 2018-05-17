6e12fb5666d3d474dd5ea1dd325abfdbce82bea5
Thijs Klooster <thijskloostert@gmail.com>
2018-05-17 18:54:36 +0200

    Update test

--- flask_monitoringdashboard/test/views/test_export_data.py
+++ flask_monitoringdashboard/test/views/test_export_data.py
@@ -36,6 +36,8 @@ class TestExportData(unittest.TestCase):
         test_results['test_runs'].append(
             {'name': 'test_1', 'exec_time': 50, 'time': str(datetime.datetime.now()), 'successful': True, 'iter': 1})
         test_results['grouped_tests'].append({'endpoint': 'endpoint_1', 'test_name': 'test_1'})
+        test_results['app_version'] = '1.0'
+        test_results['travis_job'] = '133.7'
         test_post_data(self, 'submit-test-results', test_results)
 
     def test_get_json_data_from(self):
