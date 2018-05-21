aaae3c635b012800a2184db146ac04325ec1a6f4
Thijs Klooster <thijskloostert@gmail.com>
2018-05-21 10:31:33 +0200

    Update get_rules

--- flask_monitoringdashboard/core/rules.py
+++ flask_monitoringdashboard/core/rules.py
@@ -5,6 +5,10 @@ def get_rules(end=None):
     """
     from flask_monitoringdashboard import config, user_app
 
-    rules = user_app.url_map.iter_rules(endpoint=end)
+    rules = user_app.url_map.iter_rules()
+    for rule in rules:
+        if rule.endpoint == end:
+            rules = user_app.url_map.iter_rules(endpoint=end)
+            break
     return [r for r in rules if not r.rule.startswith('/' + config.link)
             and not r.rule.startswith('/static-' + config.link)]
