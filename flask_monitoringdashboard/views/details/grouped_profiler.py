3cea725364d600a62beb5a8304401a441c503c81
Bogdan Petre <bogdan.petre0510@gmail.com>
2018-05-31 14:57:16 +0200

    Order by line number

--- flask_monitoringdashboard/views/details/grouped_profiler.py
+++ flask_monitoringdashboard/views/details/grouped_profiler.py
@@ -59,32 +59,44 @@ def grouped_profiler(end):
         details = get_endpoint_details(db_session, end)
         data = get_grouped_profiled_requests(db_session, end)
     lines = [lines for _, lines in data]
+    requests = [requests for requests, _ in data]
+    total_execution_time = 0
+    for r in requests:
+        total_execution_time += r.execution_time
     lines = reduce(lambda x, y: x + y, lines)
 
     histogram = {}
     for line in lines:
         key = (line.indent, line.line_text)
         if key in histogram:
-            histogram[key].append(line.value)
+            histogram[key].append((line.value, line.line_number))
         else:
-            histogram[key] = [line.value]
+            histogram[key] = [(line.value, line.line_number)]
 
-    total = max([sum(s) for s in histogram.values()])
+    total = max([sum(s[0]) for s in histogram.values()])
     table = []
     index = 0
     for key, values in histogram.items():
+        total_line_hits = 0
+        sum_line_number = 0
+        for v in values:
+            total_line_hits += v[0]
+            sum_line_number += v[1]
+
         indent, line_text = key
         table.append({
             'index': index,
             'indent': indent,
             'code': line_text,
             'hits': len(values),
-            'average': sum(values) / len(values),
-            'percentage': sum(values) / total
+            'average': total_execution_time / len(requests),
+            'percentage': total_line_hits / (total * len(values)),
+            'avg_ln': sum_line_number / len(values)
         })
         index += 1
 
-    total_time = [request.execution_time for request, _ in data]
-    average_time = sum(total_time) / len(total_time)
+    total_execution_time = [request.execution_time for request, _ in data]
+    average_time = sum(total_execution_time) / len(total_execution_time)
+    table = sorted(table, key=lambda row: row.get('avg_ln'))
     return render_template('fmd_dashboard/profiler_grouped.html', details=details, table=table, get_body=get_body,
                            average_time=average_time, title='Grouped Profiler results for {}'.format(end))
