04cefef7ca6d11c8d4baa934dff13c0ca8d26298
Bogdan Petre <bogdan.petre0510@gmail.com>
2018-06-05 11:03:51 +0200

    Fixed grouped profiles and endpoint summary

--- flask_monitoringdashboard/core/rules.py
+++ flask_monitoringdashboard/core/rules.py
@@ -1,11 +1,11 @@
-def get_rules(end=None):
+def get_rules(endpoint_name=None):
     """
-    :param end: if specified, only return the available rules to that endpoint
+    :param endpoint_name: if specified, only return the available rules to that endpoint
     :return: A list of the current rules in the attached Flask app
     """
     from flask_monitoringdashboard import config, user_app
     try:
-        rules = user_app.url_map.iter_rules(endpoint=end)
+        rules = user_app.url_map.iter_rules(endpoint=endpoint_name)
     except KeyError:
         return []
     return [r for r in rules if not r.rule.startswith('/' + config.link)
