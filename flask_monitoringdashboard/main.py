104f533a1eade6aa59dd280235ab64e642dca40e
Patrick Vogel <patrickvogel@live.nl>
2018-06-22 12:59:44 +0200

    updated main

--- flask_monitoringdashboard/main.py
+++ flask_monitoringdashboard/main.py
@@ -13,8 +13,6 @@ def create_app():
 
     app = Flask(__name__)
 
-    dashboard.config.outlier_detection_constant = 0
-    # dashboard.config.database_name = 'sqlite:///flask_monitoring_dashboard_v10.db'
     dashboard.config.version = '3.1'
     dashboard.bind(app)
 
@@ -24,56 +22,6 @@ def create_app():
 
     @app.route('/endpoint')
     def endpoint():
-        f()
-        return 'Ok'
-
-    @app.route('/endpoint1')
-    def endpoint1():
-        f()
-        return 'Ok'
-
-    @app.route('/endpoint2')
-    def endpoint2():
-        f()
-        return 'Ok'
-
-    @app.route('/endpoint3')
-    def endpoint3():
-        f()
-        return 'Ok'
-
-    @app.route('/endpoint4')
-    def endpoint4():
-        f()
-        return 'Ok'
-
-    @app.route('/endpoint5')
-    def endpoint5():
-        f()
-        return 'Ok'
-
-    @app.route('/endpoint6')
-    def endpoint6():
-        f()
-        return 'Ok'
-
-    @app.route('/endpoint7')
-    def endpoint7():
-        f()
-        return 'Ok'
-
-    @app.route('/endpoint8')
-    def endpoint8():
-        f()
-        return 'Ok'
-
-    @app.route('/endpoint9')
-    def endpoint9():
-        f()
-        return 'Ok'
-
-    @app.route('/endpoint10')
-    def endpoint10():
         f()
         return 'Ok'
 
