9b4b2c96ec4e10a572b143b1fa13f296b702abec
Johan de Jager <johan@laptify.nl>
2019-06-03 13:45:38 +0200

    Processed Patrick's feedback regarding retrieving params from the URL

--- flask_monitoringdashboard/static/js/controllers/statusCodeDistribution.js
+++ flask_monitoringdashboard/static/js/controllers/statusCodeDistribution.js
@@ -1,26 +1,28 @@
 function StatusCodeDistributionController($scope, $http, infoService, endpointService, menuService, formService, plotlyService, $filter) {
     menuService.reset('status_code_distribution');
+    endpointService.reset();
 
-    var matches = /endpoint\/(\d+)\/status_code_distribution/g.exec(window.location.toString());
+    formService.setReload(function () {
+        var endpointId = endpointService.info.id;
 
-    var endpointId = parseInt(matches[1]);
+        $http.get('api/endpoint_status_code_distribution/' + endpointId).then(function (response) {
+            var layout = {
+                height: 400,
+                width: 500
+            };
 
+            const statusCodes = Object.keys(response.data);
 
-    $http.get('api/endpoint_status_code_distribution/' + endpointId).then(function (response) {
-        var layout = {
-            height: 400,
-            width: 500
-        };
-
-        const statusCodes = Object.keys(response.data);
-
-        var data = [{
-            values: statusCodes.map(statusCode => response.data[statusCode]),
-            labels: statusCodes,
-            type: 'pie'
-        }];
+            var data = [{
+                values: statusCodes.map(statusCode => response.data[statusCode]),
+                labels: statusCodes,
+                type: 'pie'
+            }];
 
 
-        plotlyService.chart(data, layout);
+            plotlyService.chart(data, layout);
+        });
     });
+
+    formService.reload();
 }
