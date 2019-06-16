9138d414d720d75e5b7be839db4e5d19a745e995
Johan de Jager <johan@laptify.nl>
2019-06-16 16:01:10 +0200

    Removed code that's not supposed to be here yet

--- flask_monitoringdashboard/core/reporting/questions/question.py
+++
@@ -1,9 +0,0 @@
-from flask_monitoringdashboard.core.reporting.answer import Answer
-from abc import ABC, abstractmethod
-
-
-class Question(ABC):
-
-    @abstractmethod
-    def answer(self):
-        raise NotImplementedError()
--- flask_monitoringdashboard/core/reporting/report.py
+++
@@ -1,3 +0,0 @@
-class Report(object):
-    def __init__(self):
-        pass
