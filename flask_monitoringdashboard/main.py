ae5c1921e95e3436d1e14e12b7add49d212319c5
Mircea Lungu <mircea.lungu@gmail.com>
2019-10-10 18:59:36 +0200

    first draft

--- flask_monitoringdashboard/main.py
+++ flask_monitoringdashboard/main.py
@@ -7,11 +7,11 @@
     Note: This is not used when the flask_monitoring_dashboard
     is attached to your flask application.
 """
-import random
+from random import random
+import datetime
 import time
 
 from flask import Flask
-
 
 import flask_monitoringdashboard as dashboard
 
@@ -20,8 +20,30 @@ app = Flask(__name__)
 dashboard.config.version = '3.2'
 dashboard.config.group_by = '2'
 dashboard.config.database_name = 'sqlite:///data.db'
+
+
 # dashboard.config.database_name = 'mysql+pymysql://user:password@localhost:3306/db1'
 # dashboard.config.database_name = 'postgresql://user:password@localhost:5432/mydb'
+
+
+def on_the_minute():
+    print(f"On the minute: {datetime.datetime.now()}")
+    return int(random() * 100 // 10)
+
+
+minute_schedule = {'second': 00}
+
+dashboard.add_graph("On Half Minute", on_the_minute, "cron", **minute_schedule)
+
+
+def every_ten_seconds():
+    print(f"every_ten_seconds!!! {datetime.datetime.now()}")
+    return int(random() * 100 // 10)
+
+
+every_ten_seconds_schedule = {'seconds': 10}
+
+dashboard.add_graph("Every 10 Seconds", every_ten_seconds, "interval", **every_ten_seconds_schedule)
 
 dashboard.bind(app)
 
