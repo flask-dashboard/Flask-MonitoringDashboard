e54f0b568636c25ca99219f7deef28fb3e1833f1
Johan de Jager <johan@laptify.nl>
2019-10-13 03:15:02 +0200

    Python 2 abstract classes

--- flask_monitoringdashboard/core/reporting/questions/question.py
+++ flask_monitoringdashboard/core/reporting/questions/question.py
@@ -1,7 +1,10 @@
-from abc import ABC, abstractmethod
+
+from abc import ABCMeta, abstractmethod
 
 
-class Answer(ABC):
+class Answer:
+    __metaclass__ = ABCMeta
+
     def __init__(self, type):
         self.type = type
 
