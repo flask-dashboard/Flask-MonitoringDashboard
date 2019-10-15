cee32ba507cb88aaca88e264df330790574fdec2
Johan de Jager <johan@laptify.nl>
2019-10-15 02:11:57 +0200

    Recovered main.py

--- flask_monitoringdashboard/main.py
+++ flask_monitoringdashboard/main.py
@@ -1,28 +1,57 @@
 """
     This file can be executed for developing purposes.
-    It is not used when the flask_monitoring_dashboard is attached to an existing flask application.
+    To run use
+
+        python main.py
+
+    Note: This is not used when the flask_monitoring_dashboard
+    is attached to your flask application.
 """
-import random
+from random import random
+import datetime
 import time
 
 from flask import Flask
 
-
 import flask_monitoringdashboard as dashboard
+
 app = Flask(__name__)
 
 dashboard.config.version = '3.2'
 dashboard.config.group_by = '2'
 dashboard.config.database_name = 'sqlite:///data.db'
+
+
 # dashboard.config.database_name = 'mysql+pymysql://user:password@localhost:3306/db1'
 # dashboard.config.database_name = 'postgresql://user:password@localhost:5432/mydb'
+
+
+def on_the_minute():
+    print(f"On the minute: {datetime.datetime.now()}")
+    return int(random() * 100 // 10)
+
+
+minute_schedule = {'second': 00}
+
+dashboard.add_graph("On Half Minute", on_the_minute, "cron", **minute_schedule)
+
+
+def every_ten_seconds():
+    print(f"every_ten_seconds!!! {datetime.datetime.now()}")
+    return int(random() * 100 // 10)
+
+
+every_ten_seconds_schedule = {'seconds': 10}
+
+dashboard.add_graph("Every 10 Seconds", every_ten_seconds, "interval", **every_ten_seconds_schedule)
+
 dashboard.bind(app)
 
 
-@app.route('/endpointd')
+@app.route('/endpoint')
 def endpoint():
     print("Hello, world")
-    return 'Ok2d'
+    return 'Ok'
 
 
 @app.route('/endpoint2')
@@ -56,10 +85,5 @@ def my_func():
     # here should be something actually useful
     return 33.3
 
-# schedule = {'weeks': 0,
-#             'days': 0,
-#             'hours': 1,
-#             'minutes': 30,
-#             'seconds': 0}
-#
-# dashboard.add_graph('Graph1', lambda: my_func(), **schedule)
+
+app.run()
