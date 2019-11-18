e6a53c80e685d8bc35798e0b34160d7a36546909
Bogdan Petre <bogdan.petre0510@gmail.com>
2019-11-18 23:25:31 +0100

    Fix main file

--- flask_monitoringdashboard/main.py
+++ flask_monitoringdashboard/main.py
@@ -5,22 +5,18 @@
     Note: This is not used when the flask_monitoring_dashboard
     is attached to your flask application.
 """
-from random import random
-import datetime
 import time
+from random import random, randint
 
 from flask import Flask
 
 import flask_monitoringdashboard as dashboard
-from flask_monitoringdashboard.database import session_scope
 
 app = Flask(__name__)
 
 dashboard.config.version = '3.2'
 dashboard.config.group_by = '2'
 dashboard.config.database_name = 'sqlite:///data.db'
-
-
 # dashboard.config.database_name = 'mysql+pymysql://user:password@localhost:3306/db1'
 # dashboard.config.database_name = 'postgresql://user:password@localhost:5432/mydb'
 
@@ -30,7 +26,6 @@ def on_the_minute():
 
 
 minute_schedule = {'second': 00}
-
 dashboard.add_graph("On Half Minute", on_the_minute, "cron", **minute_schedule)
 
 
@@ -39,14 +34,14 @@ def every_ten_seconds():
 
 
 every_ten_seconds_schedule = {'seconds': 10}
-
 dashboard.add_graph("Every 10 Seconds", every_ten_seconds, "interval", **every_ten_seconds_schedule)
-
 dashboard.bind(app)
 
 
 @app.route('/endpoint')
 def endpoint():
+    # if session_scope is imported at the top of the file, the database config won't take effect
+    from flask_monitoringdashboard.database import session_scope
     with session_scope() as db_session:
         print(db_session.bind.dialect.name)
 
@@ -62,7 +57,7 @@ def endpoint2():
 
 @app.route('/endpoint3')
 def endpoint3():
-    if random.randint(0, 1) == 0:
+    if randint(0, 1) == 0:
         time.sleep(0.1)
     else:
         time.sleep(0.2)
@@ -86,4 +81,5 @@ def my_func():
     return 33.3
 
 
-app.run()
+if __name__ == "__main__":
+    app.run()
