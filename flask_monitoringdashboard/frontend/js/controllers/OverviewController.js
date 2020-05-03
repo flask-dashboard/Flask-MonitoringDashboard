6e30fc0acd518baebb8f6ba5357c782488395eb5
Johan De jager <johan@laptify.nl>
2020-05-03 18:11:22 +0200

    Pagination in overview

--- flask_monitoringdashboard/frontend/js/controllers/OverviewController.js
+++ flask_monitoringdashboard/frontend/js/controllers/OverviewController.js
@@ -9,6 +9,10 @@ export function OverviewController($scope, $http, $location, menuService, endpoi
     $scope.table = [];
     $scope.selectedItem = 2;
 
+    $scope.searchQuery = '';
+    $scope.pageSize = '10';
+    $scope.currentPage = 0;
+
     $scope.toggleHits = function () {
         $scope.isHits = !$scope.isHits;
     };
@@ -16,6 +20,43 @@ export function OverviewController($scope, $http, $location, menuService, endpoi
     $http.get('api/overview').then(function (response) {
         $scope.table = response.data;
     });
+
+    $scope.getFilteredItems = function () {
+        const start = Number($scope.currentPage) * $scope.pageSize;
+        const end = (Number($scope.currentPage) + 1) * Number($scope.pageSize);
+
+        return $scope.table
+            .filter(item => item.name.includes($scope.searchQuery))
+            .slice(start, end);
+    }
+
+    $scope.canGoBack = function () {
+        console.log('hello', $scope.currentPage);
+
+        return $scope.currentPage > 0;
+    }
+
+    $scope.canGoForward = function () {
+        const start = Number($scope.currentPage + 1) * $scope.pageSize;
+        const end = (Number($scope.currentPage + 1) + 1) * Number($scope.pageSize);
+
+        const data = $scope.table
+            .filter(item => item.name.includes($scope.searchQuery))
+            .slice(start, end);
+
+        console.log(data);
+
+        return data.length > 0;
+    }
+
+    $scope.nextPage = function () {
+        $scope.currentPage++;
+    }
+
+    $scope.previousPage = function () {
+        $scope.currentPage--;
+    }
+
 
     $scope.go = function (path) {
         $location.path(path);
@@ -29,8 +70,6 @@ export function OverviewController($scope, $http, $location, menuService, endpoi
             $scope.alertShow = !isNewestVersion($scope.pypi_version, $scope.dashboard_version);
         })
     });
-
-
 }
 
 function isNewestVersion(pypi_version, dashboard_version) {
