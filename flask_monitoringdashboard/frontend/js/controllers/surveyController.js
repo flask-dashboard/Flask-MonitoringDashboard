4a076a0bbcda125f629b83464d67252f9ccb8b32
johc <johc@itu.dk>
2023-11-10 10:27:14 +0100

    Added connection to backend

--- flask_monitoringdashboard/frontend/js/controllers/surveyController.js
+++ flask_monitoringdashboard/frontend/js/controllers/surveyController.js
@@ -1,27 +1,61 @@
 export function SurveyController($scope, $http) {
-    $scope.surveyShow = true;
-    $scope.reasons = {
-        privacy: false,
-        performance: false,
-        trust: false,
-        other: false
+    $scope.UUID = 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11'
+
+    var config = {
+        headers: {
+            'X-Parse-Application-Id': '',
+            'X-Parse-REST-API-Key': '',
+            'Content-Type': 'application/json'
+        }
     };
 
-    $scope.submitFollowUp = function() {
-        $scope.followUpShow = false;
-        
-        var feedback = [];
-        for (var key in $scope.reasons) {
-            if ($scope.reasons[key]) {
-                feedback.push(key !== 'other' ? key : { other: $scope.customReason });
-            }
-        }
-        
-        $http.post('/api/feedback', { reasons: feedback })
-            .then(function(response) {
-                console.log('Feedback sent:', response.data);
-            }, function(error) {
-                console.error('Error sending feedback:', error);
+    $scope.surveyShow = false;
+
+    $scope.fetchSurveyStatus = function () {
+        var queryParams = {
+            UUID: $scope.UUID
+        };
+
+        var queryString = encodeURIComponent(JSON.stringify(queryParams));
+
+        return $http.get(`https://parseapi.back4app.com/classes/Test1?where=${queryString}`, config)
+            .then(function (response) {
+                if (response.data.results && response.data.results.length > 0) {
+                    if (response.data.results[0].showSurvey === true) {
+                        console.log(response.data)
+                        $scope.surveyShow = true;
+                    }
+                } else {
+                    console.error('No survey records found.');
+                }
+            }, function (error) {
+                console.error('Error fetching consent:', error);
+            });
+    };
+
+    $scope.fetchSurveyStatus();
+
+    // Doesn't work yet
+    $scope.closeSurvey = function () {
+        $scope.fetchSurveyStatus()
+            .then(function (response) {
+                var survey = response.data.results.find(function (item) {
+                    return item.UUID === $scope.UUID;
+                });
+
+                if (survey && survey.objectId) {
+                    return $http.put(`https://parseapi.back4app.com/classes/Test1/${survey.objectId}`,
+                        { showSurvey: false }, config);
+                } else {
+                    throw new Error('No matching survey found.');
+                }
+            })
+            .then(function (response) {
+                console.log('Survey closed recorded:', response.data);
+                $scope.surveyShow = false;
+            })
+            .catch(function (error) {
+                console.error('Error:', error);
             });
     };
 }
--- flask_monitoringdashboard/frontend/js/controllers/telemetryController.js
+++ flask_monitoringdashboard/frontend/js/controllers/telemetryController.js
@@ -1,56 +1,69 @@
 export function TelemetryController($scope, $http) {
-    $scope.telemetryShow = true;
+    $scope.telemetryShow = false;
     $scope.followUpShow = false;
+
+    $scope.UUID = 'b0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11'
+
+    $scope.reasons = {
+        privacy: false,
+        performance: false,
+        trust: false,
+        other: false
+    };
 
     var config = {
         headers: {
             'X-Parse-Application-Id': '',
-            'X-Parse-REST-API-Key': '', 
+            'X-Parse-REST-API-Key': '',
             'Content-Type': 'application/json'
         }
     };
 
-    $scope.acceptTelemetry = function() {
+    $scope.fetchConsent = function () {
+        var queryParams = {
+            UUID: $scope.UUID
+        };
+    
+        var queryString = encodeURIComponent(JSON.stringify(queryParams));
+    
+        $http.get(`https://parseapi.back4app.com/classes/Test1?where=${queryString}`, config)
+            .then(function (response) {
+                if (response.data.results.length === 0) {
+                    $scope.telemetryShow = true
+                }
+            }, function (error) {
+                console.error('Error fetching consent:', error);
+            });
+    };
+    
+    $scope.fetchConsent();
+
+    $scope.handleTelemetry = function (consent) {
         $scope.telemetryShow = false;
-        $http.post('/api/user/telemetry_consent', { consent: true })
-            .then(function(response) {
+        $scope.followUpShow = !consent;
+
+        $http.post('https://parseapi.back4app.com/classes/Test1', { UUID: $scope.UUID, consent: consent }, config)
+            .then(function (response) {
                 console.log('Consent recorded:', response.data);
-            }, function(error) {
+            }, function (error) {
                 console.error('Error recording consent:', error);
             });
     };
 
-    $scope.declineTelemetry = function() {
-        $scope.telemetryShow = false;
-        $scope.followUpShow = true;
-        $http.post('/api/user/telemetry_consent', { consent: false })
-            .then(function(response) {
-                console.log('Consent recorded:', response.data);
-            }, function(error) {
-                console.error('Error recording consent:', error);
-            });
-    };
-
-     $scope.submitFollowUp = function() {
+    $scope.submitFollowUp = function () {
         $scope.followUpShow = false;
-        
+
         var feedback = [];
         for (var key in $scope.reasons) {
             if ($scope.reasons[key]) {
                 feedback.push(key !== 'other' ? key : { other: $scope.customReason });
             }
         }
-        
-        // Setup the data to be sent
-        var data = {
-            reasons: feedback
-        };
 
-        // Perform the POST request to the Back4App server
-        $http.post('https://parseapi.back4app.com/classes/Feedback1', data, config)
-            .then(function(response) {
+        $http.post('https://parseapi.back4app.com/classes/Test1', { UUID: $scope.UUID, reasons: feedback }, config)
+            .then(function (response) {
                 console.log('Feedback sent:', response.data);
-            }, function(error) {
+            }, function (error) {
                 console.error('Error sending feedback:', error);
             });
     };
