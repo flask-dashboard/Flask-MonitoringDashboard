2b1c4738702bf7878ce3a3f43e0198f1ee004ae3
Mircea Filip Lungu <mircea.lungu@gmail.com>
2025-02-25 21:50:22 +0100

    removed test that verifies the path to static resources. i don't think this qualifies as a unit test... also it fails after we removed the generated code from version control. we could ensure that webpack is run before the tests... but still, would that make sense?

--- tests/api/test_static.py
+++
@@ -1,8 +0,0 @@
-def test_static_css(dashboard):
-    response = dashboard.get('dashboard/static/css/main.css')
-    assert response.status_code == 200
-
-
-def test_static_js(dashboard):
-    response = dashboard.get('dashboard/static/js/app.js')
-    assert response.status_code == 200
