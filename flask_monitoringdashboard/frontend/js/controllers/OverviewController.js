511506799b0de77f024e5d64c17b3d02034471dd
Carmen Alberte Nielsen <alcn@itu.dk>
2025-02-25 14:20:54 +0100

    Parse last-accessed as dates when sorting instead of comparing as strings

--- flask_monitoringdashboard/frontend/js/controllers/OverviewController.js
+++ flask_monitoringdashboard/frontend/js/controllers/OverviewController.js
@@ -32,6 +32,9 @@ export function OverviewController($scope, $http, $location, menuService, endpoi
     });
 
     function ascendingOrder(a, b){
+      if ($scope.sortBy === 'last-accessed') {
+        return Date.parse(a[$scope.sortBy]) < Date.parse(b[$scope.sortBy]) || a[$scope.sortBy] === null;
+        }
       return a[$scope.sortBy] < b[$scope.sortBy] || a[$scope.sortBy] === null;
     }
 
