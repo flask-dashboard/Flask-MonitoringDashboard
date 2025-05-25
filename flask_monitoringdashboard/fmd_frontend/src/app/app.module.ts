66c12374e7c1dec38a14f552ff1720adb2070183
AlbertRossJoh <albertrossj@gmail.com>
2025-05-25 10:41:03 +0200

    angular version bump

--- flask_monitoringdashboard/fmd_frontend/src/app/app.module.ts
+++ flask_monitoringdashboard/fmd_frontend/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { DurationMsPipe } from './pipes/duration-ms.pipe';
 import { OverviewComponent } from './dashboard/overview/overview.component';
 import { DateDifferencePipe } from './pipes/date-difference.pipe';
@@ -11,22 +11,15 @@ import { MonitorLevelComponent } from './shared/monitor-level/monitor-level.comp
 import { FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    DurationMsPipe,
-    OverviewComponent,
-    DateDifferencePipe,
-    MonitorLevelComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
-    FontAwesomeModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [
+        AppComponent,
+        DurationMsPipe,
+        OverviewComponent,
+        DateDifferencePipe,
+        MonitorLevelComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        FontAwesomeModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
