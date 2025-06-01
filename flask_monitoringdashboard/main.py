57ffbf6e5ba0625b4d804552e99efa199747770c
AlbertRossJoh <albertrossj@gmail.com>
2025-06-01 16:33:58 +0200

    config

--- flask_monitoringdashboard/main.py
+++ flask_monitoringdashboard/main.py
@@ -19,7 +19,8 @@ app = Flask(__name__)
 
 dashboard.config.version = "3.2"
 dashboard.config.group_by = "2"
-dashboard.config.database_name = "sqlite:///data.db"
+dashboard.config.database_name = "mysql+pymysql://root:Wsf5555391@localhost/fmd"
+
 # dashboard.config.database_name = 'mysql+pymysql://user:password@localhost:3306/db1'
 # dashboard.config.database_name = 'postgresql://user:password@localhost:5432/mydb'
 
